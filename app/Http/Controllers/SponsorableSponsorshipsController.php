6537d388b2cd09604bc7d1ba3d30fa4989df477b
Francisco Daniel Garcia Noriega <pako.gn@hotmail.com>
2020-05-07 09:39:00 -0500

    Test that slots are listed in chronological order.

--- app/Http/Controllers/SponsorableSponsorshipsController.php
+++ app/Http/Controllers/SponsorableSponsorshipsController.php
@@ -9,7 +9,7 @@ class SponsorableSponsorshipsController extends Controller
     function new ($slug) {
         $sponsorable = Sponsorable::findOrFailBySlug($slug);
 
-        $sponsorableSlots = $sponsorable->slots;
+        $sponsorableSlots = $sponsorable->slots()->orderBy('publish_date')->get();
 
         return view('sponsorable-sponsorships.new', [
             'sponsorable' => $sponsorable,
--- tests/Feature/NewSponsorshipTest.php
+++ tests/Feature/NewSponsorshipTest.php
@@ -6,6 +6,7 @@ use App\Sponsorable;
 use App\SponsorableSlot;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class NewSponsorshipTest extends TestCase
@@ -32,5 +33,25 @@ class NewSponsorshipTest extends TestCase
         $response->assertSuccessful();
         $this->assertTrue($response->data('sponsorable')->is($sponsorable));
         $sponsorableSlots->assertEquals($response->data('sponsorableSlots'));
+    }
+
+    public function test_sponsorable_slots_are_listed_in_chronological_order()
+    {
+        $sponsorable = factory(Sponsorable::class)->create(['slug' => 'full-stack-radio']);
+
+        $sponsorableSlots = new EloquentCollection([
+            $slotA = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(10), 'sponsorable_id' => $sponsorable]),
+            $slotB = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(30), 'sponsorable_id' => $sponsorable]),
+            $slotC = factory(SponsorableSlot::class)->create(['publish_date' => Carbon::now()->addDays(3), 'sponsorable_id' => $sponsorable]),
+        ]);
+
+        $response = $this->get('/full-stack-radio/sponsorships/new');
+
+        $response->assertSuccessful();
+        $this->assertTrue($response->data('sponsorable')->is($sponsorable));
+        $this->assertCount(3, $response->data('sponsorableSlots'));
+        $this->assertTrue($response->data('sponsorableSlots')[0]->is($slotC));
+        $this->assertTrue($response->data('sponsorableSlots')[1]->is($slotA));
+        $this->assertTrue($response->data('sponsorableSlots')[2]->is($slotB));
     }
 }
